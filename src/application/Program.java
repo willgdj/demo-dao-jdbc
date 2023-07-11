a1d76247e00c7fa43ceb424e977d7f4c42584fa5
willgdj <120098718+willgdj@users.noreply.github.com>
2023-07-11 08:14:25 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,13 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -42,5 +45,12 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n=== Test 6: update insert ====");
+        System.out.println("Enter id for delete test:  ");
+        int id = sc.nextInt();sellerDao.deleteById(id);
+        System.out.println("Deleted completed");
+
+        sc.close();
     }
 }
